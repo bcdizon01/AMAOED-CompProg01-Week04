a8b6b538f19e1cf09b04463e8f99622255d7e8d6
bcdizon01 <57063225+bcdizon01@users.noreply.github.com>
2019-10-28 00:06:20 +0800

    Add files via upload

---
+++ src/Week004/hello1.cpp
@@ -0,0 +1,37 @@
+/* ****************************************************************
+* @author: Bryan Jesus Dizon
+ * @app name: Hello World
+ * @app desc: CS201 - Week004
+ * @history:
+ *  - <2019/10/27> | hello1
+ *      -- created app
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+cout << "Hello\n";
+cout << "World!\n";
+
+ return EXIT_SUCCESS;
+
+}
